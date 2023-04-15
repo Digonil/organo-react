ae3ca0386840798a12f6cf2b209a8a400e13b9d9
Digonil <diego.niles@hotmail.com>
2023-04-15 18:20:20 -0300

    Criado componente de colaborador e rodapé

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Banner from './componentes/Banner/Banner.js';
 import Formulario from './componentes/Formulario/index.js';
 import Time from './componentes/Time/index.js';
+import Rodape from './componentes/Rodape/Rodape.js';
 
 function App() {
 
@@ -62,11 +63,15 @@ function App() {
       <Banner/>
       <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
 
-      {times.map(time => <Time key={time.nome} 
+      {times.map(time => <Time 
+        key={time.nome} 
         nome={time.nome} 
         corPrimaria={time.corPrimaria} 
-        corSecundaria={time.corSecundaria}/>
+        corSecundaria={time.corSecundaria}
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+        />
         )}
+        <Rodape/>
       
     </div>
   );
