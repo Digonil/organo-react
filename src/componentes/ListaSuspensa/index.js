ea571c093026aa6f0d9ba0f912171500bb1a6175
Digonil <diego.niles@hotmail.com>
2023-04-15 13:16:45 -0300

    Criado comportamento de receber os valores das variáveis no formulário e o componente time

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,12 +1,11 @@
 import '/home/digonil/Documents/cursos_frontend/organo/src/componentes/ListaSuspensa/ListaSupensa.css'
 
 const ListaSuspensa = (props) => {
-    console.log(props.itens)
-    
+
     return(
         <div className='lista-suspensa'>
             <label>{props.label}</label>
-            <select required={props.required}>
+            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required} value={props.value}>
                 {props.itens.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
